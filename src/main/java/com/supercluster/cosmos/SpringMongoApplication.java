c96648bb67128d5738061bc7f93ae940780027df
Karthik <springcoding@gmail.com>
2018-12-29 22:10:21 -0500

    Added local mongo connection string

--- src/main/java/com/supercluster/cosmos/SpringMongoApplication.java
+++ src/main/java/com/supercluster/cosmos/SpringMongoApplication.java
@@ -1,50 +1,12 @@
 package com.supercluster.cosmos;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.supercluster.cosmos.model.Customer;
-import com.supercluster.cosmos.repository.CustomerRepository;
 
 @SpringBootApplication
 public class SpringMongoApplication {
 
-	@Autowired
-	private CustomerRepository repository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongoApplication.class, args);
 	}
-
-	//@Override
-	public void donotrun(String... args) throws Exception {
-
-		repository.deleteAll();
-
-		// save a couple of customers
-		repository.save(new Customer("Alice", "Smith"));
-		repository.save(new Customer("Bob", "Smith"));
-
-		// fetch all customers
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (Customer customer : repository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println();
-
-		// fetch an individual customer
-		System.out.println("Customer found with findByFirstName('Alice'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByFirstName("Alice"));
-
-		System.out.println("Customers found with findByLastName('Smith'):");
-		System.out.println("--------------------------------");
-		for (Customer customer : repository.findByLastName("Smith")) {
-			System.out.println(customer);
-		}
-
-	}
-
 }
